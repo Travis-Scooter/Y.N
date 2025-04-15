b82ebe204708d1f332550c910c94f2ce1f87a565
Travis Scooter <zoltan.daniel.monori@umszki.hu>
2025-04-15 09:56:30 +0200

    Szorgalom munka kész.

---
+++ szorgalom.py
@@ -0,0 +1,26 @@
+def getNum(msg : str = "Kérek egy számot: "):
+    try:
+        return int(input(msg))
+    except:
+        print("Ez nem egy szám, kérek újat...")
+        return getNum(msg)
+
+osztalyzat = getNum("A szorgalom osztályzata: ")
+
+szorgalom : str
+
+match osztalyzat:
+    case 1:
+        szorgalom = "rossz"
+    case 2:
+        szorgalom = "hanyag"
+    case 3:
+        szorgalom = "változó"
+    case 4:
+        szorgalom = "jó"
+    case 5:
+        szorgalom = "példás"
+    case other:
+        szorgalom = "ez nem egy osztályzat"
+
+print("A szorgalom: " + szorgalom + "!")
